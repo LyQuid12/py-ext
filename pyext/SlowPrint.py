f05359fadb91eb339c7eb5204c82a09b25f2ea6c
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2021-12-28 18:40:37 +0700

    Create SlowPrint.py for SlowPrint function

---
+++ pyext/SlowPrint.py
@@ -0,0 +1,17 @@
+import sys
+from time import sleep
+
+
+class SlowPrint:
+    def __init__(self, text, delay:int=0.5):
+        r"""
+        A class that implements the server side
+        -----
+        Parameters :
+        - text: :class:`str` | Text you wanna print
+        - delay: :class:`int` | How long does it take for the text to print. The bigger the number, the longer the text will print (Default : 0.5)
+        """
+        for letter in text:
+            sys.stdout.write(letter)
+            sys.stdout.flush()
+            sleep(delay)
