dbb4cf5dbb5f156035b730bc2885d195d1e7c3ff
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2021-12-28 18:43:08 +0700

    Update __init__.py

--- pyext/__init__.py
+++ pyext/__init__.py
@@ -3,6 +3,6 @@ from .SlowPrint import *
 
 __title__ = "py-ext"
 __version__ = "1.0"
-__authors__ = ["LyQuid"]
+__authors__ = "LyQuid"
 __license__ = "MIT License"
 __copyright__ = "Copyright 2021 LyQuid"
